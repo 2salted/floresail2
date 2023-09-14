bd7d5495b41e1435d537b0fe39e277dd66ce3cd6
2salted <131868085+2salted@users.noreply.github.com>
2023-09-14 20:01:44 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 var image1 = document.getElementById("heroImage");
 var image2 = document.getElementById("heroImage2");
 var image3 = document.getElementById("heroImage3");
+var bigtxt = document.getElementById("bigtxt");
 var currentImage = 1;
-
+/*
 function toggleImages() {
     if (currentImage === 1) {
+        // Slide to the second image
         image1.style.opacity = 0;
         image2.style.opacity = 1;
         currentImage = 2;
     } else if (currentImage === 2) {
+        // Slide to the third image
         image2.style.opacity = 0;
         image3.style.opacity = 1;
         currentImage = 3;
     } else {
+        // Slide back to the first image and update text color to white
         image3.style.opacity = 0;
         image1.style.opacity = 1;
         currentImage = 1;
@@ -20,7 +24,7 @@ function toggleImages() {
 }
 
 setInterval(toggleImages, 10000); // Switch images every 10 seconds
-
+*/
 
 function myFunction(x) {
     x.classList.toggle("change");
@@ -57,7 +61,7 @@ mobileMenuItems.forEach(function (item) {
 document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function () {
         document.querySelector(".text-container").style.color = "#0d3b54";
-        document.querySelector(".text-container").style.transform = "translate(-58%, -50%)";
+        document.querySelector(".text-container").style.transform = "translate(-50%, -50%)";
         
         document.querySelectorAll("#smalltxt, #bigtxt, #undertxt").forEach(function (element, index) {
             setTimeout(function () {
